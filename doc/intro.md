dc099846ce4c15d8afeb9eda9587945907949119
Cris Jr <crisjr@pm.me>
2021-11-01 08:56:32 -0300

    Remove unnecessary files

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to task-manager
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- test/task_manager/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns task-manager.core-test
-  (:require [clojure.test :refer :all]
-            [task-manager.core :refer :all]))
-
-(deftest a-test
-  (testing "I work now."
-    (is (= 1 1))))
